04fc8548b2bbf6cdcd6c4d1f6db2b561da4a2e0d
Abhishek-FreeZ <abhishekkumarak1998@gmail.com>
2022-05-29 15:50:34 +0530

    Fibonacci Using Recursion

---
+++ src/FibonacciWithRecursion.java
@@ -0,0 +1,15 @@
+public class FibonacciWithRecursion {
+
+    public static int fibonacci(int num){
+        if(num<0)
+            return 0;
+        else if(num==0)
+            return 1;
+        else
+            return num*fibonacci(num-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(10));
+    }
+}
